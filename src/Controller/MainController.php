87c9c76970105c6ab8b0fed75c39650e23feef16
accn <neudeck@amiconsult.de>
2024-10-03 17:17:14 +0200

    feat: add non-autowireable services

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Repository\StarshipRepository;
+use Symfony\Bridge\Twig\Command\DebugCommand;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -18,7 +21,14 @@ class MainController extends AbstractController
         StarshipRepository  $starshipRepository,
         HttpClientInterface $client,
         CacheInterface $issLocationPool,
+        #[Autowire(param: 'iss_location_cache_ttl')]
+        int $issLocationCacheTtl,
+        #[Autowire(service:'twig.command.debug')]
+        DebugCommand $twigDebugCommand,
     ): Response {
+        $output = new BufferedOutput();
+        $twigDebugCommand->run(new ArrayInput([]), $output);
+        dd($output);
         $ships = $starshipRepository->findAll();
         $myShip = $ships[array_rand($ships)];
 
